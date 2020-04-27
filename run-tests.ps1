79ac50f41b9edfa5c49b937effbf0d96b7ac455d
Will <willherring1@gmail.com>
2020-04-27 11:43:26 +0100

    Removed newman folder deletion from shell scripts

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,5 +1,3 @@
-Remove-Item .\newman -Recurse
-
 $collection='room_controller_tests.postman_collection'
 $environment='.\environment_variables.postman_environment'
 $output=$(newman run $collection -e $environment -r htmlextra --reporter-htmlextra-title "RBP Postman Test Report")
