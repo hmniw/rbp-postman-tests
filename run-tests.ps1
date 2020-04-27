8501f6b4e1bfff7cccb3a568afa5602c808f6aca
Will <willherring1@gmail.com>
2020-04-27 11:39:24 +0100

    Added powershell script since we're on Windows (duh!), and added gitignore

---
+++ run-tests.ps1
@@ -0,0 +1,7 @@
+Remove-Item .\newman -Recurse
+
+$collection='room_controller_tests.postman_collection'
+$environment='.\environment_variables.postman_environment'
+$output=$(newman run $collection -e $environment -r htmlextra --reporter-htmlextra-title "RBP Postman Test Report")
+
+Write-Host $output
