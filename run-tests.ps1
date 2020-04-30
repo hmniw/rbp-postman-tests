94be4ea9050144fab5e710afde184f0671ff783e
Will <willherring1@gmail.com>
2020-04-30 12:42:35 +0100

    Modified report name

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,5 +1,5 @@
 $collection='room_controller_tests.postman_collection'
 $environment='environment_variables.postman_environment'
-$output=$(newman run $collection -e $environment -r htmlextra --reporter-htmlextra-export newman/report.html --reporter-htmlextra-title "RBP Postman Test Report")
+$output=$(newman run $collection -e $environment -r htmlextra --reporter-htmlextra-export newman/report.html --reporter-htmlextra-title "RBP Room Controller Tests")
 
 Write-Host $output
