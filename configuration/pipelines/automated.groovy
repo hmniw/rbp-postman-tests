6da5975b30a09e818caa1bbc911d00548039bfb1
Will <willherring1@gmail.com>
2020-04-30 10:56:10 +0100

    Added pipeline script for running in Jenkins

---
+++ configuration/pipelines/automated.groovy
@@ -0,0 +1,24 @@
+CREDENTIALS_ID = 'github'
+
+try {
+
+    node( 'master' ) {
+
+        stage( 'Initialise' ) {
+            git url: 'https://github.com/hmniw/rbp-postman-tests.git', branch: 'master', credentialsId: CREDENTIALS_ID
+        }
+
+        stage( 'Install Dependencies' ) {
+            powershell 'npm install -g newman'
+            powershell 'npm install'
+        }
+
+        stage( 'Run Tests' ) {
+            powershell 'run-tests.ps1'
+        }
+
+    }
+} catch (exception) {
+    currentBuild.result = 'FAILURE'
+    throw exception
+}
--- run-tests.ps1
+++ run-tests.ps1
@@ -1,5 +1,5 @@
 $collection='room_controller_tests.postman_collection'
-$environment='.\environment_variables.postman_environment'
+$environment='environment_variables.postman_environment'
 $output=$(newman run $collection -e $environment -r htmlextra --reporter-htmlextra-title "RBP Postman Test Report")
 
 Write-Host $output
