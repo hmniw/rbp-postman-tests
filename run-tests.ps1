ae71375448d133b9435f7f1a9fcaefc8fb0f8f22
Will <willherring1@gmail.com>
2020-05-11 09:00:03 +0100

    Added additional test for non-auth creations

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,5 +1,5 @@
-$collection='room_controller_tests.postman_collection'
-$environment='environment_variables.postman_environment'
+$collection='room_controller_tests.postman_collection.json'
+$environment='environment_variables.postman_environment.json'
 $output=$(newman run $collection -e $environment -r htmlextra --reporter-htmlextra-export newman/report.html --reporter-htmlextra-title "RBP Room Controller Tests")
 
 Write-Host $output
